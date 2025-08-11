cd0e649d9cc3818968db86eb7bd89cabcbe3206c
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-11 19:20:51 -0300

    Projeto iniciado e arquivo app.py criado

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Olá, mundo! Este é o nosso Blog!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
