c4f3d250a3278c0ee4babe3dfb1182b09b0b70a2
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-11 19:50:37 -0300

    feat: Adiciona rota dinâmica para exibir posts individuais

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Lista de posts temporária
+posts = [
+    {'titulo': 'Meu Primeiro Post', 'autor': 'João'},
+    {'titulo': 'Aprendendo Python', 'autor': 'Maria'}
+]
+
 @app.route("/")
 def index():
-    posts = [
-        {'titulo': 'Meu Primeiro Post', 'autor': 'João'},
-        {'titulo': 'Aprendendo Python', 'autor': 'Maria'}
-    ]
     return render_template("index.html", posts=posts)
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    return f"Você está visualizando o post de ID {post_id}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
