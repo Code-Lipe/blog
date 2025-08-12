7524f88b8d468a091af7783b7f0433200605e358
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-12 18:26:02 -0300

    feat: Adiciona o modelo de dados de postagem em models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(50), nullable=False)
+    conteudo = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
